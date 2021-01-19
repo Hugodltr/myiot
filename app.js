820bb74255f2158f37b1af096538f4ed48156e86
Vincent DELPECH <vincent.delpech@epfedu.fr>
2021-01-19 11:37:17 +0100

    add photo to covid

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ async function app(res) {
 
     let vegetables, covidAlert, airQuality;
 
-    results = await connection.query('SELECT * FROM vegetables LIMIT 10');
+    results = await connection.query('SELECT * FROM vegetables1 LIMIT 10');
     vegetables = results[0];
 
-    results = await connection.query('SELECT * FROM vegetables LIMIT 10');
+    results = await connection.query('SELECT * FROM vegetables1 LIMIT 10');
     vegetables = results[0];
 
-    results = await connection.query('SELECT * FROM covidAlert LIMIT 10');
+    results = await connection.query('SELECT * FROM covidAlert ORDER BY timestamp DESC LIMIT 10');
     covidAlert = results[0];
 
     results = await connection.query('SELECT * FROM airQuality LIMIT 10');
     airQuality = results[0];
 
-    results = await connection.query('SELECT * FROM test_image LIMIT 10');
+    results = await connection.query('SELECT * FROM test_image LIMIT 10 ');
     let image = results[0]
 
     res.render('./index.ejs', {
